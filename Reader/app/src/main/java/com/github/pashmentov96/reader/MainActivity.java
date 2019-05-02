a7b32bf616715a106d34b48bc872cbc7bc57318b
nikita <nik.pashmentov@ya.ru>
2019-05-02 15:39:19 +0300

    Реализация открытия файла. При нажатии на файл в лог пишется путь к файлу

--- Reader/app/src/main/java/com/github/pashmentov96/reader/MainActivity.java
+++ Reader/app/src/main/java/com/github/pashmentov96/reader/MainActivity.java
@@ -1,7 +1,12 @@
 package com.github.pashmentov96.reader;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,13 +16,15 @@ public class MainActivity extends AppCompatActivity {
     Button buttonHistory;
     Button buttonOpen;
 
+    final int PICK_FILE_REQUEST = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonHistory = (Button)findViewById(R.id.button_history);
-        buttonOpen = (Button)findViewById(R.id.button_open);
+        buttonHistory = findViewById(R.id.button_history);
+        buttonOpen = findViewById(R.id.button_open);
 
         View.OnClickListener onClickButtonHistory = new View.OnClickListener() {
             @Override
@@ -32,11 +39,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(v.getContext(), "Click on open_button", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent();
+                intent.setType("*/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Choose file to open"), PICK_FILE_REQUEST);
             }
         };
 
         buttonOpen.setOnClickListener(onClickButtonOpen);
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == PICK_FILE_REQUEST) {
+                if (data == null) {
+                    return ;
+                }
+
+                Uri selectedFileUri = data.getData();
+                String path = selectedFileUri.getPath();
+                Log.d("MyLogs", "Path: " + path);
+                Log.d("MyLogs", "Data: " + data.getDataString());
+                //Toast.makeText(this, "Path: " + path + " + " + data.getDataString(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
