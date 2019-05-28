188864c3d7e31b9471d07c86312e718cea6e1382
nikita <nik.pashmentov@ya.ru>
2019-05-28 18:22:55 +0300

    Добавление нового экрана для отображения содержания книги

---
+++ Reader/app/src/main/java/com/github/pashmentov96/reader/ScreenSlidePageFragment.java
@@ -0,0 +1,49 @@
+package com.github.pashmentov96.reader;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ScreenSlidePageFragment extends Fragment {
+    static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
+    static final String ARGUMENT_PAGE_TEXT = "arg_page_text";
+    int pageNumber;
+    String text;
+
+    public static ScreenSlidePageFragment newInstance(int page, String textOfPage) {
+        ScreenSlidePageFragment screenSlidePageFragment = new ScreenSlidePageFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(ARGUMENT_PAGE_NUMBER, page);
+        arguments.putString(ARGUMENT_PAGE_TEXT, textOfPage);
+        screenSlidePageFragment.setArguments(arguments);
+        return screenSlidePageFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNumber = getArguments().getInt(ARGUMENT_PAGE_NUMBER);
+        text = getArguments().getString(ARGUMENT_PAGE_TEXT);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_screen_slide_page, container, false);
+
+        ClickableTextView page = rootView.findViewById(R.id.textOfPage);
+        page.setText("Page " + pageNumber + "\n" + text);
+
+        Log.d("Pages", text);
+
+        page.setTextWithAllWords();
+
+        return rootView;
+    }
+}
