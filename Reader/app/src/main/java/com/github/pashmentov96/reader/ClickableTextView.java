490b81a3f52edd394ee162033827305ab6a084fc
nikita <nik.pashmentov@ya.ru>
2019-05-31 11:46:15 +0300

    Добавить перевод слова

--- Reader/app/src/main/java/com/github/pashmentov96/reader/ClickableTextView.java
+++ Reader/app/src/main/java/com/github/pashmentov96/reader/ClickableTextView.java
@@ -1,6 +1,8 @@
 package com.github.pashmentov96.reader;
 
 import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.text.SpannableStringBuilder;
 import android.text.TextPaint;
@@ -10,6 +12,15 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ClickableTextView extends android.support.v7.widget.AppCompatTextView {
     public ClickableTextView(Context context) {
@@ -68,24 +79,76 @@ public class ClickableTextView extends android.support.v7.widget.AppCompatTextVi
     public static class ClickableWord {
         private String word;
         private ClickableSpan clickableSpan;
+
         public ClickableWord(final String word, final int color, final TextView clickedWord, final TextView translationOfWord) {
             this.word = word;
             this.clickableSpan = new ClickableSpan() {
                 @Override
                 public void onClick(@NonNull View widget) {
                     clickedWord.setText(word);
-                    translationOfWord.setText("Translation of " + word);
                     Log.d("MyLogs", "Click on " + word);
+                    new AsyncTask<Void, Void, String>() {
+                        @Override
+                        protected void onPostExecute(String s) {
+                            translationOfWord.setText(parseFromJson(s));
+                            Log.d("MyLogs", "json = " + s);
+                        }
+
+                        @Override
+                        protected String doInBackground(Void... voids) {
+                            return translate(word);
+                        }
+                    }.execute();
                 }
 
                 @Override
                 public void updateDrawState(@NonNull TextPaint ds) {
-                    //super.updateDrawState(ds);
                     ds.setUnderlineText(false);
                     ds.setColor(color);
                 }
             };
         }
+
+        private String parseFromJson(String json) {
+            try {
+                JSONObject jsonObject = new JSONObject(json);
+                return jsonObject.getJSONArray("text").get(0).toString();
+            } catch (JSONException e) {
+                return "Error";
+            }
+        }
+
+        private String translate(String word) {
+            String key = "trnsl.1.1.20190531T071741Z.3ddfb9a0422e3948.53a88c2bd5fb9d0ef621d39e03e76ca81ddaf681";
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=en-ru&key=" + key + "&text=" + word);
+                Log.d("MyLogs", "URL = " + url);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+
+                Log.d("MyLogs", "Code " + connection.getResponseCode() + "; " + "Message " + connection.getResponseMessage());
+                InputStream content = connection.getInputStream();
+                BufferedReader in =
+                        new BufferedReader (new InputStreamReader(content));
+
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = in.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+                return stringBuilder.toString();
+            } catch(Exception e) {
+                e.printStackTrace();
+                return "Error";
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+        }
+
 
         /**
          * @return the word
--- Reader/app/src/main/java/com/github/pashmentov96/reader/ScreenSlidePagerActivity.java
+++ Reader/app/src/main/java/com/github/pashmentov96/reader/ScreenSlidePagerActivity.java
@@ -2,6 +2,8 @@ package com.github.pashmentov96.reader;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,6 +14,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
